da0aee9fb19c8623a040042c39d9a5fe70ce224a
Endo Axelsson <endo.axelsson@elev.ga.ntig.se>
2024-01-23 11:12:02 +0100

    id options

--- routes/index.js
+++ routes/index.js
@@ -26,10 +26,16 @@ router.post('/username', function (req, res) {
 
 const pool = require('../db')
 
-router.get('/dbtest', async (req, res) => {
+router.get('/dbtest/:id', async (req, res) => {
   try {
-    const [parts] = await pool.promise().query('SELECT * FROM endo_part')
-    res.json({ parts })
+    const id = req.params.id
+    const [parts] = await pool
+    .promise()
+    .query(`SELECT * FROM endo_part WHERE id = ${id}`)
+    const [options] = await pool
+    .promise()
+    .query(`SELECT * FROM endo_option WHERE id = ${id}`)
+    res.json({ parts, options })
   } catch (error) {
     console.log(error)
     res.sendStatus(500)
